e1f004467a23fc87ca65793421ce824e374fd2fb
Shibzel <contact@shibzel.com>
2024-01-13 16:23:05 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-#python3 -m venv .venv
-#./.venv/bin/activate
+VENV_ROOT=~/.venvs
+SCRIPT_VENV_PATH=$VENV_ROOT/dynamic-ip-cloudflare
+
+if [ ! -d $SCRIPT_VENV_PATH ]; then
+    if [ ! -d $VENV_ROOT ]; then
+        mkdir -p $VENV_ROOT
+    fi
+    python3 -m venv $SCRIPT_VENV_PATH
+fi
+
+source $SCRIPT_VENV_PATH/bin/activate
 
 python3 -m pip install -r requirements.txt
 
